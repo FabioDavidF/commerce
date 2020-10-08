24fc23d3141b3e81a07a2dd4a3188faa71fec864
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-10-08 20:43:25 -0300

    Minor changes to models (NM)

--- auctions/models.py
+++ auctions/models.py
@@ -9,8 +9,8 @@ class Listing(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     starting_bid = models.FloatField()
-    img_url = models.URLField(blank=True)
-    category = models.CharField(max_length=64)
+    img_url = models.URLField(Null=True)
+    category = models.CharField(max_length=64, Null=True)
 
     def __str__(self):
         return self.title
